aa3138cb9ab4cac8e520449a9b7b043320aa3520
Sumama <sumamabutt500@gmail.com>
2025-06-25 15:46:57 +0500

    ca

--- api/cards.js
+++ api/cards.js
@@ -1,17 +1,18 @@
-import { connectDB } from "../utils/db.js";
-import { getCardsByAdress } from "../controllers/card.controller.js";
+import { connectDB } from "../../../utils/db.js";
+import { getCardsByAdress } from "../../../controllers/card.controller.js";
 
 export default async function handler(req, res) {
   await connectDB();
+  const { method, url } = req;
+  const cleanUrl = url.split("?")[0];
 
-  if (req.method === "GET") {
-    const { page, section } = req.query;
+  try {
+    if (method === "POST" && cleanUrl.endsWith("/getCardsByAdress")) {
+      return getCardsByAdress(req, res);
+    }
 
-    // Set req.params to mimic Express-style behavior if needed in controller
-    req.params = { page, section };
-
-    return getCardsByAdress(req, res);
+    res.status(405).json({ message: "Method Not Allowed" });
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error", error: error.message });
   }
-
-  res.status(405).json({ message: "Method Not Allowed" });
 }
