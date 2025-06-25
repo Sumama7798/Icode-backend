f4933a27907301aaaad1f64d5860ea4af1573777
Sumama <sumamabutt500@gmail.com>
2025-06-25 15:47:50 +0500

    l

--- api/message.js
+++ api/message.js
@@ -3,10 +3,16 @@ import { sendEmail } from "../controllers/message.controller.js";
 
 export default async function handler(req, res) {
   await connectDB();
+  const { method, url } = req;
+  const cleanUrl = url.split("?")[0];
 
-  if (req.method === "POST") {
-    return sendEmail(req, res);
+  try {
+    if (method === "POST" && cleanUrl.endsWith("/sendEmail")) {
+      return sendEmail(req, res);
+    }
+
+    res.status(405).json({ message: "Method Not Allowed" });
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error", error: error.message });
   }
-
-  res.status(405).json({ message: "Method Not Allowed" });
 }
